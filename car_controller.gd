b8539e32244da4ae7b79484a3d4d3c05cd306803
Johannes Krieg <johannes-daniel.krieg@smail.th-koeln.de>
2026-01-24 03:35:05 +0100

    add acceleration for testing

--- car_controller.gd
+++ car_controller.gd
@@ -1,11 +1,9 @@
 extends VehicleBody3D
 
 @export var MAX_STEER = 0.7
+@export var ENGINE_POWER = 100
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 func _physics_process(delta):
 	steering = move_toward(steering, Input.get_axis("Steer Right", "Steer Left") * MAX_STEER, delta * 5)
+	engine_force = Input.get_axis("Down", "Up") * ENGINE_POWER
