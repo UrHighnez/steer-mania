8a2b44fb90ee92dbb0d9401da4dd9c9becf835c5
Johannes Krieg <johannes-daniel.krieg@smail.th-koeln.de>
2026-01-24 04:43:26 +0100

    update wheels (they finally work)

--- car_controller.gd
+++ car_controller.gd
@@ -1,9 +1,9 @@
 extends VehicleBody3D
 
 @export var MAX_STEER = 0.7
-@export var ENGINE_POWER = 100
+@export var ENGINE_POWER = 300
 
 
 func _physics_process(delta):
 	steering = move_toward(steering, Input.get_axis("Steer Right", "Steer Left") * MAX_STEER, delta * 5)
-	engine_force = Input.get_axis("Down", "Up") * ENGINE_POWER
+	engine_force = Input.get_axis("Up", "Down") * ENGINE_POWER
